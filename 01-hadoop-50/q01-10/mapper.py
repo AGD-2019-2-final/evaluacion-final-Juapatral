0cf433fa4fb9fac2601f793767859b8da9b25970
juapatral <juapatral@gmail.com>
2020-01-30 15:43:02 -0500

    Commit all task

--- 01-hadoop-50/q01-10/mapper.py
+++ 01-hadoop-50/q01-10/mapper.py
@@ -1,4 +1,10 @@
+#! /usr/bin/env python
 import sys
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+infile = sys.stdin
+for line in infile:
+    line = line.split(',')
+    line = line[2]
+    sys.stdout.write("{}\t1\n".format(line))
